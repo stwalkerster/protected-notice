57a863617bc6ab5ba085bebd82ab08d2a6d31600
Simon Walker <github@stwalkerster.co.uk>
2012-04-19 05:09:19 +0000

    styling

--- ProtectedNotice.i18n.php
+++ ProtectedNotice.i18n.php
@@ -6,12 +6,8 @@ $messages['en'] = array(
         // Extension description message
 	'protectednotice-desc' => "Displays a pre-defined message on all protected pages",
 
-	'protectednotice-notice' => '{| style="background:red;"
-|-
-|This page is protected:
-|-
-| $1
-|}',
+	'protectednotice-notice' => '<table style="margin:4px 10%;border-collapse:collapse;border:2px solid #bba;background:#f9f9f9;"><tr><td style="border:none;padding:0px;width:1px;"></td><td style="border:none;padding:0.25e  0.9em;width:100%;"> This page is protected:
+$1 </td></tr></table>',
 	'protectednotice-restriction' => '* From $1 (requires $2)',
 
 	// protection levels
